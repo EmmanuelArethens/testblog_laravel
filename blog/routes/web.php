a9c28d8812a447d680eee813aea358c20142b24c
EmmanuelArethens <emmanuel.arethens1@gmail.com>
2018-04-03 06:21:38 -0400

    project initialized + route article

---
+++ blog/routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/article', function(){
+    return ($articles = [
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    "Vivamus id massa ac ex rutrum vestibulum.",
+    "Nam purus justo, porttitor vel urna id, blandit aliquam orci."
+]);
+});
+
+Route::get('/article/{index}', function () {
+    return view('welcome');
+});
